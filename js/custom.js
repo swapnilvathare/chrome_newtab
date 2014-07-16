533c094d2fd42d2aba6401f7621f2308a183843c
Swapnil Vathare <swapnil@weboniselab.com>
2014-07-16 17:26:51 +0530

    google search help done

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
         url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D2295412&format=json&diagnostics=true&callback=',
         dataType: 'json',
         success: function (data) {
-            //console.log(data.feed.entries);
-            //alert(data.weather[0]["description"]);
-            //alert(data.query.results.channel.item.condition.temp);
             var fahrenheit = data.query.results.channel.item.condition.temp;
             var weatherImg = data.query.results.channel.item.condition.code;
             var celsius = (fahrenheit-32)*5/9;
@@ -26,16 +23,14 @@ $(document).ready(function () {
                 var celsiusL = (forecasts.low-32)*5/9;
                 var celsiusLRound = Math.round( celsiusL * 1 ) / 1 + "°";
                 var itemHtml = '<li class="condition wc-showers"><span class="name">'+forecasts.day+'</span><span class="tempIcon"><img alt="Mostly Cloudy" src="images/plain_weather/flat_white/svg/'+forecasts.code+'.svg"></span><span class="temperature"><span class="hi-c">'+celsiusHRound+'</span><span class="lo-c">'+celsiusLRound+'</span></span><div class="clear"></div></li>'
-                //var item_html = '<li class="col-md-6"><div class="flip-container" ontouchstart="this.classList.toggle("hover");"><div class="flipper"><div class="front"><div class="panel panel-success"><div class="panel-heading"><h3 class="panel-title"><a target="_blank" href="' + entry.link + '">' + entry.title + '</a></h3>  </div><div class="panel-body">'+ entry.contentSnippet +'</div></div></div><div class="back detailNews">'+ entry.content +'</div></div></div></li>';
-                //var item_html = '<li class="col-md-4"><div class="flip-container" ontouchstart="this.classList.toggle("hover");"><div class="flipper"><div class="front" style="background:url(images/WindowsLogo.jpg)"><h3 class="panel-title"><a target="_blank" href="' + entry.link + '">' + entry.title + '</a></h3></div><div class="back">'+ entry.contentSnippet +'</div></div></div></li>';
-                // var item_html = '<li class="col-md-6"><div class="panel panel-success"><div class="panel-heading"><h3 class="panel-title"><a target="_blank" href="' + entry.link + '">' + entry.title + '</a></h3>  </div>  <div class="panel-body">'+ entry.contentSnippet +'</div></div></li>';
-                //var item_html = '<li class="col-md-4"><a target="_blank" href="' + entry.link + '">' + entry.contentSnippet + '</a></li>';
+                
                 $('ul.forecast').append(itemHtml);
             });
         },
         error: function () {}
 
     });
+
     $('.search').click(function(){
         var searchText = $('.searchField').val();
         console.log(searchText);
@@ -48,6 +43,9 @@ $(document).ready(function () {
         }
     })
 
+    //http://googlesuggest-jquery.googlecode.com/svn/trunk/demo.html
+    $('input.searchField').googleSuggest({ service: 'web' });
+    
     $(document).keypress(function(e) {
         if(e.which == 13) {
             console.log('You pressed enter!');
